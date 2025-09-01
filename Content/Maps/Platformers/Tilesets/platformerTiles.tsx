c3e61333b60efadfbc48168f3e76afc7d4570508
byteKnight2000 <rogerramirez0331@gmail.com>
2025-09-01 15:34:57 -0700

    Add files via upload

---
+++ Content/Maps/Platformers/Tilesets/platformerTiles.tsx
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="platformerTiles" tilewidth="96" tileheight="96" tilecount="39" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../Images/TileImages/Brick.png" width="96" height="96"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../Images/TileImages/BrickStart.png" width="96" height="96"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../Images/TileImages/Lantern.png" width="96" height="96"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../Images/TileImages/LanternInactive.png" width="96" height="96"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../Images/TileImages/LifeBlock.png" width="96" height="96"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../Images/TileImages/LifeBlockInactive.png" width="96" height="96"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../Images/TileImages/LifeBlockSwitch.png" width="96" height="96"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../Images/TileImages/OrbStart.png" width="96" height="96"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../Images/TileImages/OrbEnd.png" width="96" height="96"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../Images/TileImages/OrbEndSwitch.png" width="96" height="96"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../Images/TileImages/SwitchBlockOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../Images/TileImages/SwitchBlockOff.png" width="96" height="96"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../Images/TileImages/TeleportPortalOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../Images/TileImages/TeleportLocationOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../Images/TileImages/TeleportLocationOff.png" width="96" height="96"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../Images/TileImages/TeleportPortalOff.png" width="96" height="96"/>
+ </tile>
+ <tile id="37">
+  <image source="../../../Images/TileImages/SpikeBlock.png" width="96" height="96"/>
+ </tile>
+ <tile id="22">
+  <image source="../../../Images/TileImages/SpikeL.png" width="96" height="96"/>
+ </tile>
+ <tile id="31">
+  <image source="../../../Images/TileImages/SpikeR.png" width="96" height="96"/>
+ </tile>
+ <tile id="36">
+  <image source="../../../Images/TileImages/SpikeU.png" width="96" height="96"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../Images/TileImages/SpikeD.png" width="96" height="96"/>
+ </tile>
+ <tile id="17">
+  <image source="../../../Images/TileImages/SpikeFallingBlock.png" width="96" height="96"/>
+ </tile>
+ <tile id="19">
+  <image source="../../../Images/TileImages/SpikeFallingL.png" width="96" height="96"/>
+ </tile>
+ <tile id="20">
+  <image source="../../../Images/TileImages/SpikeFallingR.png" width="96" height="96"/>
+ </tile>
+ <tile id="21">
+  <image source="../../../Images/TileImages/SpikeFallingU.png" width="96" height="96"/>
+ </tile>
+ <tile id="18">
+  <image source="../../../Images/TileImages/SpikeFallingD.png" width="96" height="96"/>
+ </tile>
+ <tile id="24">
+  <image source="../../../Images/TileImages/SpikeOffL.png" width="96" height="96"/>
+ </tile>
+ <tile id="25">
+  <image source="../../../Images/TileImages/SpikeOffR.png" width="96" height="96"/>
+ </tile>
+ <tile id="26">
+  <image source="../../../Images/TileImages/SpikeOffU.png" width="96" height="96"/>
+ </tile>
+ <tile id="23">
+  <image source="../../../Images/TileImages/SpikeOffD.png" width="96" height="96"/>
+ </tile>
+ <tile id="28">
+  <image source="../../../Images/TileImages/SpikeOnL.png" width="96" height="96"/>
+ </tile>
+ <tile id="29">
+  <image source="../../../Images/TileImages/SpikeOnR.png" width="96" height="96"/>
+ </tile>
+ <tile id="30">
+  <image source="../../../Images/TileImages/SpikeOnU.png" width="96" height="96"/>
+ </tile>
+ <tile id="27">
+  <image source="../../../Images/TileImages/SpikeOnD.png" width="96" height="96"/>
+ </tile>
+ <tile id="33">
+  <image source="../../../Images/TileImages/SpikeSpringL.png" width="96" height="96"/>
+ </tile>
+ <tile id="34">
+  <image source="../../../Images/TileImages/SpikeSpringR.png" width="96" height="96"/>
+ </tile>
+ <tile id="35">
+  <image source="../../../Images/TileImages/SpikeSpringU.png" width="96" height="96"/>
+ </tile>
+ <tile id="32">
+  <image source="../../../Images/TileImages/SpikeSpringD.png" width="96" height="96"/>
+ </tile>
+ <tile id="38">
+  <image source="../../../Images/TileImages/SpikeFallingPlaceholder.png" width="96" height="96"/>
+ </tile>
+</tileset>
