82b88ad6d33acf55db2bea5dd140fd1c1f9074f8
byteKnight2000 <rogerramirez0331@gmail.com>
2025-09-01 15:19:01 -0700

    Updated main.cs

--- code/main.cs
+++ code/main.cs
@@ -1,112 +1,111 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Xml.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using static Cube_Run_C_.Globals;
 
 
 namespace Cube_Run_C_ {
-  public static class Level {
-    private static Dictionary<int, Texture2D> GidTextureCache = new();
-    public static List<Vector2> TeleportLocations = new() { new(0, 0), new(0, 0), new(0, 0) };
-    public static (Dimensions, Dimensions) Dimensions = new(new(0, 0), new(0, 0));
-    public static ushort Gravity = 800;
-    public static bool Active = false;
+    public class Main : Game {
+        private Dimensions StoredScreenDimensions;
+        private Point StoredWindowPosition;
+        private GraphicsDeviceManager Graphics;
+        private SpriteBatch SpriteBatch;
+        private bool InFullScreen = false;
 
 
-    public static void Reset(string mapPath) {
-      Level.GidTextureCache.Clear();
-      Level.TeleportLocations = new();
-      Level.Dimensions = new(new(0, 0), new(0, 0));
-      Level.Gravity = 800;
-      Level.Active = true;
+        public Main() {
+            this.Graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+        }
 
-      Level.Setup(mapPath);
+
+        protected override void Initialize() {
+            this.Graphics.PreferredBackBufferWidth = DEFAULT_DIMENSIONS.Width;
+            this.Graphics.PreferredBackBufferHeight = DEFAULT_DIMENSIONS.Height;
+            this.Graphics.SynchronizeWithVerticalRetrace = true;
+            this.Graphics.ApplyChanges();
+
+            Camera.Graphics = Graphics;
+            Assets.GraphicsDevice = Graphics.GraphicsDevice;
+            MonitorDimensions = new(GraphicsDevice.Adapter.CurrentDisplayMode.Width, GraphicsDevice.Adapter.CurrentDisplayMode.Height);
+
+            Camera.Reset(Color.Teal);
+
+            this.IsFixedTimeStep = true;
+            this.TargetElapsedTime = TimeSpan.FromSeconds(1.0 / 144.0);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            this.SpriteBatch = new SpriteBatch(GraphicsDevice);
+            Assets.Content = this.Content;
+            Assets.SpriteBatch = this.SpriteBatch;
+            Level.Reset($"Content/Maps/Platformers/{PlayerData.CurrentLevel}.tmx");
+        }
+
+
+        private void FullScreen() {
+            this.InFullScreen = !this.InFullScreen;
+
+            if (this.InFullScreen) {
+                this.StoredScreenDimensions.Width = this.Graphics.PreferredBackBufferWidth;
+                this.StoredScreenDimensions.Height = this.Graphics.PreferredBackBufferHeight;
+                this.StoredWindowPosition = this.Window.Position;
+                this.Graphics.PreferredBackBufferWidth = MonitorDimensions.Width;
+                this.Graphics.PreferredBackBufferHeight = MonitorDimensions.Height;
+
+                this.Window.IsBorderless = true;
+                this.Window.Position = new Point(0, 0);
+            } else {
+                this.Graphics.PreferredBackBufferWidth = this.StoredScreenDimensions.Width;
+                this.Graphics.PreferredBackBufferHeight = this.StoredScreenDimensions.Height;
+                this.Window.IsBorderless = false;
+                this.Window.Position = this.StoredWindowPosition;
+            }
+
+            this.Graphics.ApplyChanges();
+            Camera.UpdateScale();
+        }
+
+
+        protected override void Draw(GameTime gameTime) {
+            this.GraphicsDevice.Clear(Camera.BackgroundColor);
+
+            this.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+            Vector2 drawposition = Level.Active ? Globals.Player.Rect.Center() : new(96, 96);
+            Camera.Draw(SpriteBatch, drawposition);
+
+            this.SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        protected override void Update(GameTime gameTime) {
+            InputManager.Update(true, true);
+
+            float DELTA_TIME = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            CurrentGameTime = gameTime.TotalGameTime;
+
+            if (InputManager.IsButtonPressed(Buttons.Back) || InputManager.IsKeyDown(Keys.Escape))
+                Exit();
+            if (InputManager.IsKeyPressed(Keys.F)) {
+                this.FullScreen();
+            }
+
+            if (Level.Active) {
+                foreach (SpriteGroup<Sprite> SpriteGroup in SpriteGroups.Values) {
+                    SpriteGroup.Update(DELTA_TIME);
+                }
+
+                Globals.Player.Update(DELTA_TIME);
+            }
+
+
+            base.Update(gameTime);
+        }
     }
-
-
-    private static void Setup(string mapPath) {
-      (ZLayers, List<string>)[] LayerProperties = [(ZLayers.Background, new() { "All" }), (ZLayers.Main, new() { "All", "Collidable" }), (ZLayers.Foreground, new() { "All" }), (ZLayers.Main, new() { "All", "Damage" })];
-      string[] TileLayers = ["Background", "Terrain", "Foreground", "Enemies"];
-      string[] ObjectLayers = ["Interactable", "Interactable Enemies", "Collectable", "Moving Objects"];
-      XDocument TmxMap = XDocument.Load(mapPath);
-      Dimensions LevelDimensions = new(int.Parse(TmxMap.Root.Attribute("width").Value), int.Parse(TmxMap.Root.Attribute("height").Value));
-      string MapDirectory = Path.GetDirectoryName(mapPath);
-      byte Index = 0;
-
-      LevelData.Dimensions.Item1 = LevelDimensions;
-      LevelData.Dimensions.Item2 = new(LevelDimensions.Width * TILE_SIZE, LevelDimensions.Height * TILE_SIZE);
-
-      foreach (XElement Tileset in TmxMap.Descendants("tileset")) {
-        foreach (XElement tile in XDocument.Load(Path.GetFullPath(Path.Combine(MapDirectory, Tileset.Attribute("source").Value))).Root.Elements("tile")) {
-          XElement TileImageElement = tile.Element("image");
-
-          if (TileImageElement == null) continue;
-
-          Level.GidTextureCache[int.Parse(Tileset.Attribute("firstgid").Value) + int.Parse(tile.Attribute("id").Value)] = Assets.GetTexture($"Images/TileImages/{Path.GetFileNameWithoutExtension(TileImageElement.Attribute("source").Value)}");
-        }
-      }
-
-      foreach (XElement TileLayer in TmxMap.Descendants("layer")) {
-        if (Index >= LayerProperties.Length) break;
-
-        string[] Gids = TileLayer.Element("data").Value.Trim().Split(new[] { ',', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-        (ZLayers, List<string>) LayerProperty = LayerProperties[Index];
-        Dimensions LayerDimensions = new(int.Parse(TileLayer.Attribute("width").Value), int.Parse(TileLayer.Attribute("height").Value));
-
-        for (int Y = 0; Y < LayerDimensions.Height; Y++) {
-          for (int X = 0; X < LayerDimensions.Width; X++) {
-            int Gid = int.Parse(Gids[Y * LayerDimensions.Width + X]);
-
-            if (Gid == 0) continue;
-
-            new Sprite(Level.GidTextureCache.OrderByDescending(kvp => kvp.Key).First(kvp => kvp.Key <= Gid).Value, new(X * TILE_SIZE, Y * TILE_SIZE), LayerProperty.Item2, LayerProperty.Item1);
-          }
-        }
-
-        Index++;
-      }
-      
-      foreach (XElement ObjectLayer in TmxMap.Descendants("objectgroup")) {
-        string LayerName = ObjectLayer.Attribute("name")?.Value ?? "";
-
-        foreach (XElement Object in ObjectLayer.Elements("object")) {
-          Vector2 Position = new(int.Parse(Object.Attribute("x").Value), int.Parse(Object.Attribute("y").Value) - TILE_SIZE);
-          XAttribute GidAttribute = Object.Attribute("gid");
-          Texture2D Image = null;
-          string Name = Object.Attribute("name")?.Value ?? "";
-
-          if (GidAttribute != null && Level.GidTextureCache.TryGetValue(int.Parse(GidAttribute.Value), out Texture2D Texture)) {
-            Image = Texture;
-          }
-
-          switch (LayerName) {
-            case "Interactable":
-              switch (Name) {
-                case "StartBall":
-                  new Sprite(Image, Position, new() { "All" }, ZLayers.Main);
-                  Globals.Player = new Player(Position);
-                  break;
-                case "EndBall":
-
-                  break;
-              }
-              break;
-            case "Interactable Enemies":
-
-              break;
-            case "Collectable":
-
-              break;
-            case "Moving Object":
-
-              break;
-          }
-        }
-      }
-    }
-  }
 }
