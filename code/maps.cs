c3413d5bcf14c7aa8c62bbbc29d2062b95cda56a
byteKnight2000 <rogerramirez0331@gmail.com>
2025-09-27 19:35:29 -0700

    Update maps.cs

--- code/maps.cs
+++ code/maps.cs
@@ -5,57 +5,83 @@ using System.Linq;
 using System.Xml.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using static Cube_Run_C_.Tools;
 using static Cube_Run_C_.Globals;
+using static Cube_Run_C_.Tools.Assets;
+using static Cube_Run_C_.Tools.Engine;
+using static Cube_Run_C_.Tools.BitMask;
+using static Cube_Run_C_.Globals.PlayerData;
+using RectangleF = System.Drawing.RectangleF;
 
 
 namespace Cube_Run_C_ {
+  public static class LevelController {
+    public static void BeginLevel(byte level) {
+      if (CurrentLevel != level) CurrentLevel = level;
+
+      Level.Reset();
+      Level.Setup($"Content/Maps/Platformers/{level}.tmx");
+    }
+  }
+
   public static class Level {
+    private static Dictionary<string, (ZLayers, List<Groups>)> TileLayerProperties = new() {
+      ["Terrain"] = (ZLayers.Main, new() { Groups.All, Groups.Collidable }),
+      ["Enemies"] = (ZLayers.Main, new() { Groups.All, Groups.Damage }),
+      ["Foreground"] = (ZLayers.Foreground, new() { Groups.All }),
+      ["Background"] = (ZLayers.Background, new() { Groups.All })
+    };
     private static Dictionary<int, Texture2D> GidTextureCache = new();
-    public static List<Vector2> TeleportLocations = new() { new(0, 0), new(0, 0), new(0, 0) };
-    public static (Dimensions, Dimensions) Dimensions = new(new(0, 0), new(0, 0));
+    public static Vector2[] TeleportLocations = [Vector2.Zero, Vector2.Zero, Vector2.Zero];
+    public static readonly string[] TileLayers = ["Terrain", "Enemies", "Foreground", "Backgroud"];
+    public static readonly string[] ObjectLayers = ["Interactable", "Interactable Enemies", "Collectable", "Moving Objects"];
+    private static RectangleF PlayerRect;
     public static ushort Gravity = 800;
     public static bool Active = false;
 
 
-    public static void Reset(string mapPath) {
-      Level.GidTextureCache.Clear();
-      Level.TeleportLocations = new();
-      Level.Dimensions = new(new(0, 0), new(0, 0));
-      Level.Gravity = 800;
-      Level.Active = true;
+    public static void Reset() {
+      Camera.Reset(Color.Teal);
 
-      Level.Setup(mapPath);
+      Globals.Player = null;
+      for (int Index = 0; Index < SpriteGroups.Length; Index++) {
+        SpriteGroups[Index].Clear();
+      }
+
+      GidTextureCache.Clear();
+      TeleportLocations = [Vector2.Zero, Vector2.Zero, Vector2.Zero];
+      Gravity = 800;
+      Active = false;
     }
 
 
-    private static void Setup(string mapPath) {
-      (ZLayers, List<string>)[] LayerProperties = [(ZLayers.Background, new() { "All" }), (ZLayers.Main, new() { "All", "Collidable" }), (ZLayers.Foreground, new() { "All" }), (ZLayers.Main, new() { "All", "Damage" })];
-      string[] TileLayers = ["Background", "Terrain", "Foreground", "Enemies"];
-      string[] ObjectLayers = ["Interactable", "Interactable Enemies", "Collectable", "Moving Objects"];
+    public static void Setup(string mapPath) {
       XDocument TmxMap = XDocument.Load(mapPath);
+      XElement[] MapTilesets = [.. TmxMap.Descendants("tileset")];
+      XElement[] MapTileLayers = [.. TmxMap.Descendants("layer")];
+      XElement[] MapObjectLayers = [.. TmxMap.Descendants("objectgroup")];
       Dimensions LevelDimensions = new(int.Parse(TmxMap.Root.Attribute("width").Value), int.Parse(TmxMap.Root.Attribute("height").Value));
+      Vector2 PlayerSpawnPosition = new();
       string MapDirectory = Path.GetDirectoryName(mapPath);
-      byte Index = 0;
 
-      LevelData.Dimensions.Item1 = LevelDimensions;
-      LevelData.Dimensions.Item2 = new(LevelDimensions.Width * TILE_SIZE, LevelDimensions.Height * TILE_SIZE);
 
-      foreach (XElement Tileset in TmxMap.Descendants("tileset")) {
-        foreach (XElement tile in XDocument.Load(Path.GetFullPath(Path.Combine(MapDirectory, Tileset.Attribute("source").Value))).Root.Elements("tile")) {
+      string GetObjectProperty(IEnumerable<XElement> objectProperties, string propertyName) => objectProperties.FirstOrDefault(property => property.Attribute("name").Value == propertyName)?.Attribute("value").Value;
+
+
+      void SetupTileset(XElement tileset) {
+        foreach (XElement tile in XDocument.Load(Path.GetFullPath(Path.Combine(MapDirectory, tileset.Attribute("source").Value))).Root.Elements("tile")) {
           XElement TileImageElement = tile.Element("image");
 
           if (TileImageElement == null) continue;
 
-          Level.GidTextureCache[int.Parse(Tileset.Attribute("firstgid").Value) + int.Parse(tile.Attribute("id").Value)] = Assets.GetTexture($"Images/TileImages/{Path.GetFileNameWithoutExtension(TileImageElement.Attribute("source").Value)}");
+          GidTextureCache[int.Parse(tileset.Attribute("firstgid").Value) + int.Parse(tile.Attribute("id").Value)] = GetTexture($"Images/TileImages/{Path.GetFileNameWithoutExtension(TileImageElement.Attribute("source").Value)}");
         }
       }
 
-      foreach (XElement TileLayer in TmxMap.Descendants("layer")) {
-        if (Index >= LayerProperties.Length) break;
-
-        string[] Gids = TileLayer.Element("data").Value.Trim().Split(new[] { ',', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-        (ZLayers, List<string>) LayerProperty = LayerProperties[Index];
-        Dimensions LayerDimensions = new(int.Parse(TileLayer.Attribute("width").Value), int.Parse(TileLayer.Attribute("height").Value));
+      void SetupTileLayer(XElement tileLayer) {
+        string[] Gids = tileLayer.Element("data").Value.Trim().Split(new[] { ',', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        (ZLayers, List<Groups>) LayerProperty = TileLayerProperties[tileLayer.Attribute("name").Value];
+        Dimensions LayerDimensions = new(int.Parse(tileLayer.Attribute("width").Value), int.Parse(tileLayer.Attribute("height").Value));
 
         for (int Y = 0; Y < LayerDimensions.Height; Y++) {
           for (int X = 0; X < LayerDimensions.Width; X++) {
@@ -63,43 +89,66 @@ namespace Cube_Run_C_ {
 
             if (Gid == 0) continue;
 
-            new Sprite(Level.GidTextureCache.OrderByDescending(kvp => kvp.Key).First(kvp => kvp.Key <= Gid).Value, new(X * TILE_SIZE, Y * TILE_SIZE), LayerProperty.Item2, LayerProperty.Item1);
+            new Sprite(GidTextureCache.OrderByDescending(kvp => kvp.Key).First(kvp => kvp.Key <= Gid).Value, new(X * TILE_SIZE, Y * TILE_SIZE), LayerProperty.Item2, LayerProperty.Item1);
           }
         }
-
-        Index++;
       }
-      
-      foreach (XElement ObjectLayer in TmxMap.Descendants("objectgroup")) {
-        string LayerName = ObjectLayer.Attribute("name")?.Value ?? "";
 
-        foreach (XElement Object in ObjectLayer.Elements("object")) {
-          Vector2 Position = new(int.Parse(Object.Attribute("x").Value), int.Parse(Object.Attribute("y").Value) - TILE_SIZE);
+      void SetupObjectLayer(XElement objectLayer) {
+        string LayerName = objectLayer.Attribute("name")?.Value ?? "";
+
+        foreach (XElement Object in objectLayer.Elements("object")) {
+          IEnumerable<XElement> ObjectProperties = Object.Element("properties")?.Elements("property") ?? [];
           XAttribute GidAttribute = Object.Attribute("gid");
+          Vector2 Position = new(int.Parse(Object.Attribute("x").Value), int.Parse(Object.Attribute("y").Value) - TILE_SIZE);
           Texture2D Image = null;
+          Directions FaceDirection = StringToDirection((GetObjectProperty(ObjectProperties, "Orientation") ?? "L")[0]);
           string Name = Object.Attribute("name")?.Value ?? "";
 
-          if (GidAttribute != null && Level.GidTextureCache.TryGetValue(int.Parse(GidAttribute.Value), out Texture2D Texture)) {
+
+          if (GidAttribute != null && GidTextureCache.TryGetValue(int.Parse(GidAttribute.Value), out Texture2D Texture))
             Image = Texture;
-          }
+          
 
           switch (LayerName) {
             case "Interactable":
+              List<Groups> AssignedGroups = new() { Groups.All };
+
               switch (Name) {
                 case "StartBall":
-                  new Sprite(Image, Position, new() { "All" }, ZLayers.Main);
-                  Globals.Player = new Player(Position);
+                  PlayerSpawnPosition = Position;
                   break;
                 case "EndBall":
-
+                  AssignedGroups.Add(Groups.Orb);
+                  break;
+                case "Teleport Start Location":
+                  new Teleporter(Image, Position, byte.Parse(GetObjectProperty(ObjectProperties, "ID")), true);
+                  continue;
+                case "Teleport End Location":
+                  TeleportLocations[byte.Parse(GetObjectProperty(ObjectProperties, "ID"))] = Position;
+                  break;
+                case "OnSwitchBlock":
+                  AssignedGroups.Add(Groups.Collidable);
+                  break;
+                case "Ladder":
+                  AssignedGroups.Add(Groups.Ladder);
                   break;
               }
+
+              new Sprite(Image, Position, AssignedGroups, ZLayers.Main, FaceDirection);
               break;
             case "Interactable Enemies":
 
               break;
             case "Collectable":
-
+              switch (Name) {
+                case "LifeBlock":
+                  new Item(Image, Position, new() { Groups.All, Groups.Item }, ZLayers.Main, Name, true);
+                  break;
+                case "Switch":
+                  new Item(Image, Position, new() { Groups.All, Groups.Switch }, ZLayers.Main, Name, true);
+                  break;
+              }
               break;
             case "Moving Object":
 
@@ -107,6 +156,78 @@ namespace Cube_Run_C_ {
           }
         }
       }
+
+
+      Active = true;
+      LevelData.Dimensions.Item1 = LevelDimensions;
+      LevelData.Dimensions.Item2 = new(LevelDimensions.Width * TILE_SIZE, LevelDimensions.Height * TILE_SIZE);
+
+      for (int Index = 0; Index < MapTilesets.Length; Index++) {
+        SetupTileset(MapTilesets[Index]);
+      }
+      for (int Index = 0; Index < MapTileLayers.Length; Index++) {
+        SetupTileLayer(MapTileLayers[Index]);
+      }
+      for (int Index = 0; Index < MapObjectLayers.Length; Index++) {
+        SetupObjectLayer(MapObjectLayers[Index]);
+      }
+
+      Globals.Player = new(PlayerSpawnPosition);
+    }
+
+
+    private static void CollectableCollision() {
+      (Sprite, bool) CollisionResult = SpriteGroups[(uint)Groups.Item].OverlapsWith(PlayerRect);
+
+      if (CollisionResult.Item2 && CollisionResult.Item1 is Item Item && Item.Active) {
+        switch (Item.Name) {
+          case "LifeBlock":
+            PlayerData.Lives++;
+            break;
+        }
+
+        Item.Deactivate();
+      }
+    }
+
+    private static void InteractableCollision() {
+      if (SpriteGroups[(uint)Groups.Orb].OverlapsWith(PlayerRect).Item2) {
+        PlayerData.CurrentLevel++;
+        LevelController.BeginLevel(PlayerData.CurrentLevel);
+      }
+
+      if (SpriteGroups[(uint)Groups.Ladder].OverlapsWith(PlayerRect).Item2) {
+        Globals.Player.MovementChange(PlayerData.PlayerMovers.Ladder);
+      }
+
+      for (int Index = 0; Index < SpriteGroups[(int)Groups.Teleporter].SpriteList.Count; Index++) {
+        if (SpriteGroups[(int)Groups.Teleporter].SpriteList[Index] is Teleporter TeleportPortal && TeleportPortal.Active && TeleportPortal.Rect.IntersectsWith(PlayerRect))
+          Globals.Player.Teleport(TeleportPortal);
+      }
+    }
+
+    private static void EnemyCollision() {
+      if (SpriteGroups[(int)Groups.Damage].OverlapsWith(PlayerRect).Item2) {
+        if (IsSet(Globals.Player.Stats, (byte)PlayerStats.Shielding)) {
+
+        } else {
+          Globals.Player.Death();
+        }
+      }
+    }
+
+
+    public static void Update(float deltaTime) {
+      for (int Index = 0; Index < SpriteGroups.Length; Index++) {
+        SpriteGroups[Index].Update(deltaTime);
+      }
+
+      Globals.Player.Update(deltaTime);
+      PlayerRect = Globals.Player.Rect;
+
+      CollectableCollision();
+      InteractableCollision();
+      EnemyCollision();
     }
   }
 }
