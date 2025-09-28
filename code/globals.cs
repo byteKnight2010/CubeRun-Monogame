a820d33cae4257ac7ec12dcbc3f6ddd26bf12106
byteKnight2000 <rogerramirez0331@gmail.com>
2025-09-27 19:35:13 -0700

    Update globals.cs

--- code/globals.cs
+++ code/globals.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+using static Cube_Run_C_.Tools;
 
 
 namespace Cube_Run_C_ {
@@ -15,6 +13,19 @@ namespace Cube_Run_C_ {
       Player,
       Foreground,
       UI
+    }
+
+    public enum Groups : byte {
+      Damage,
+      Item,
+      Collidable,
+      SemiCollidable,
+      Orb,
+      Teleporter,
+      Ladder,
+      Spring,
+      Switch,
+      All
     }
 
     public enum Directions : byte {
@@ -34,24 +45,68 @@ namespace Cube_Run_C_ {
       Shield
     }
 
-    public enum PlayerTimers : byte {
-      RespawnInvincibility,
-      Invincibility,
-      AutoMove,
-      Flying,
-      Frozen,
-      Goggles,
-      Honey,
-      Telescope,
-      WallJump,
-      WallJumpStun,
-      DeathStun,
-      SpringMove,
-      ShieldKnockback
-    }
-
+    
     
     public static class PlayerData {
+      [Flags]
+      public enum PlayerSurfaces : byte {
+        Left = 1 << 0,
+        Right = 1 << 1,
+        Top = 1 << 2,
+        Bottom = 1 << 3,
+        StickingCeiling = 1 << 4
+      }
+
+      [Flags]
+      public enum PlayerStats : byte {
+        ReturnedMovement = 1 << 0,
+        ReturnMovement = 1 << 1,
+        FallDamageEnabled = 1 << 2,
+        FallDamageCondition = 1 << 3,
+        Shielding = 1 << 4,
+        CanJump = 1 << 5,
+        HorizontalMovement = 1 << 6,
+        VerticalMovement = 1 << 7
+      }
+
+      [Flags]
+      public enum PlayerMovers : byte {
+        Ladder = 1 << 0,
+        Water = 2 << 1,
+        DeepWater = 3 << 2,
+        ThickWater = 4 << 3,
+        Quicksand = 5 << 4,
+        DeepQuicksand = 6 << 5
+      }
+      
+      [Flags]
+      public enum PlayerPowers : byte {
+        Invincibility = 1 << 0,
+        AutoMove = 1 << 1,
+        Flying = 1 << 2,
+        Frozen = 1 << 3,
+        Goggles = 1 << 4,
+        Honey = 1 << 5,
+        Sprint = 1 << 6,
+        Telescope = 1 << 7
+      }
+
+      public enum PlayerTimers : byte {
+        RespawnStatus,
+        Invincibility,
+        AutoMove,
+        Flying,
+        Frozen,
+        Goggles,
+        Honey,
+        Telescope,
+        WallJump,
+        WallJumpStun,
+        SpringMove,
+        ShieldKnockback
+      }
+
+
       public static ushort Lives { get; set; } = 5;
       public static ushort Coins { get; set; } = 0;
       public static byte KeyCoins { get; set; } = 0;
@@ -65,27 +120,15 @@ namespace Cube_Run_C_ {
     }
 
 
-    public static Dictionary<string, SpriteGroup<Sprite>> SpriteGroups = new() {
-      ["Damage"] = new(),
-      ["Collidable"] = new(),
-      ["Semi-Collidable"] = new(),
-      ["Orb"] = new(),
-      ["Teleporter"] = new(),
-      ["Spring"] = new()
-    };
-
-    public static Dictionary<char, char> OppositeDirections = new() {
-      ['L'] = 'R',
-      ['R'] = 'L',
-      ['U'] = 'D',
-      ['D'] = 'U'
-    };
+    public static SpriteGroup<Sprite>[] SpriteGroups = [new(), new(), new(), new(), new(), new(), new(), new(), new()];
 
 
     public static Dimensions MonitorDimensions;
     public static TimeSpan CurrentGameTime;
     public static Player Player;
-    public static readonly Dimensions DEFAULT_DIMENSIONS = new(960, 720);
+    public static readonly Dimensions DEFAULT_DIMENSIONS = new(1280, 720);
+    public static readonly Dimensions MINIMUM_DIMENSIONS = new(160, 90);
+    public static readonly float SCREEN_RATIO = 16f / 9f;
     public const byte TILE_SIZE = 96;
     public const byte CELL_SIZE = 192;
     public const byte ANIMATION_SPEED = 6;
