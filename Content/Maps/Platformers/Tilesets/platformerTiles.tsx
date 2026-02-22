11c30eb36f36d7584ed1f9a6a47807ce252fa024
byteKnight2000 <rogerramirez0331@gmail.com>
2026-02-22 12:01:53 -0800

    Add files via upload

---
+++ Content/Maps/Platformers/Tilesets/platformerTiles.tsx
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="platformerTiles" tilewidth="96" tileheight="96" tilecount="46" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../Images/TileImages/Brick.png" width="96" height="96"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../Images/TileImages/StartTile.png" width="96" height="96"/>
+ </tile>
+ <tile id="47">
+  <image source="../../../Images/CollectableImages/LifeBlockOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="48">
+  <image source="../../../Images/CollectableImages/LifeBlockSwitch.png" width="96" height="96"/>
+ </tile>
+ <tile id="52">
+  <image source="../../../Images/CollectableImages/Lantern.png" width="96" height="96"/>
+ </tile>
+ <tile id="51">
+  <image source="../../../Images/CollectableImages/LanternOff.png" width="96" height="96"/>
+ </tile>
+ <tile id="50">
+  <image source="../../../Images/CollectableImages/SwitchOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../Images/TileImages/StartOrb.png" width="96" height="96"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../Images/TileImages/OrbEnd.png" width="96" height="96"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../Images/TileImages/OrbEndSwitch.png" width="96" height="96"/>
+ </tile>
+ <tile id="63">
+  <image source="../../../Images/TileImages/OrbEndFragment.png" width="96" height="96"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../Images/TileImages/SwitchBlockOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../Images/TileImages/SwitchBlockOff.png" width="96" height="96"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../Images/TileImages/TeleportPortalOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../Images/TileImages/TeleportLocationOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="64">
+  <image source="../../../Images/TileImages/TeleportLocationOff.png" width="96" height="96"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../Images/TileImages/TeleportPortalOff.png" width="96" height="96"/>
+ </tile>
+ <tile id="58">
+  <image source="../../../Images/TileImages/Spring.png" width="96" height="96"/>
+ </tile>
+ <tile id="37">
+  <image source="../../../Images/TileImages/SpikeBlock.png" width="96" height="96"/>
+ </tile>
+ <tile id="22">
+  <image source="../../../Images/TileImages/SpikeL.png" width="96" height="96"/>
+ </tile>
+ <tile id="31">
+  <image source="../../../Images/TileImages/SpikeR.png" width="96" height="96"/>
+ </tile>
+ <tile id="36">
+  <image source="../../../Images/TileImages/SpikeU.png" width="96" height="96"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../Images/TileImages/SpikeD.png" width="96" height="96"/>
+ </tile>
+ <tile id="17">
+  <image source="../../../Images/TileImages/SpikeFallingBlock.png" width="96" height="96"/>
+ </tile>
+ <tile id="44">
+  <image source="../../../Images/TileImages/SpikeFalling.png" width="96" height="96"/>
+ </tile>
+ <tile id="45">
+  <image source="../../../Images/TileImages/SpikeOff.png" width="96" height="96"/>
+ </tile>
+ <tile id="46">
+  <image source="../../../Images/TileImages/SpikeOn.png" width="96" height="96"/>
+ </tile>
+ <tile id="34">
+  <image source="../../../Images/TileImages/SpikeSpringR.png" width="96" height="96"/>
+ </tile>
+ <tile id="55">
+  <image source="../../../Images/EnemyImages/FloorCanon.png" width="96" height="96"/>
+ </tile>
+ <tile id="57">
+  <image source="../../../Images/EnemyImages/Canon.png" width="96" height="96"/>
+ </tile>
+ <tile id="42">
+  <image source="../../../Images/EnemyImages/Prowler.png" width="96" height="96"/>
+ </tile>
+ <tile id="41">
+  <image source="../../../Images/EnemyImages/DeathCube.png" width="96" height="96"/>
+ </tile>
+ <tile id="43">
+  <image source="../../../Images/EnemyImages/DeadDeathCube.png" width="96" height="96"/>
+ </tile>
+ <tile id="70">
+  <image source="../../../Images/TileImages/Quicksand.png" width="96" height="96"/>
+ </tile>
+ <tile id="72">
+  <image source="../../../Images/TileImages/QuicksandDeep.png" width="96" height="96"/>
+ </tile>
+ <tile id="71">
+  <image source="../../../Images/TileImages/QuicksandBlock.png" width="96" height="96"/>
+ </tile>
+ <tile id="65">
+  <image source="../../../Images/TileImages/BSC.png" width="96" height="96"/>
+ </tile>
+ <tile id="66">
+  <image source="../../../Images/TileImages/ESC.png" width="96" height="96"/>
+ </tile>
+ <tile id="67">
+  <image source="../../../Images/TileImages/LWC.png" width="96" height="96"/>
+ </tile>
+ <tile id="69">
+  <image source="../../../Images/TileImages/PowerCanceller.png" width="96" height="96"/>
+ </tile>
+ <tile id="49">
+  <image source="../../../Images/CollectableImages/Checkpoint.png" width="96" height="96"/>
+ </tile>
+ <tile id="53">
+  <image source="../../../Images/CollectableImages/Coin.png" width="96" height="96"/>
+ </tile>
+ <tile id="59">
+  <image source="../../../Images/CollectableImages/PoisonBottle.png" width="96" height="96"/>
+ </tile>
+ <tile id="60">
+  <image source="../../../Images/CollectableImages/SmallPower.png" width="96" height="96"/>
+ </tile>
+ <tile id="61">
+  <image source="../../../Images/CollectableImages/InvinciblePower.png" width="96" height="96"/>
+ </tile>
+ <tile id="62">
+  <image source="../../../Images/CollectableImages/LargePower.png" width="96" height="96"/>
+ </tile>
+</tileset>
